874ef2da17aa26418bd29aa59519989918848a8b
DiMeCaCe <100180240+DiMeCaCe@users.noreply.github.com>
2023-01-05 18:15:36 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+    <h3>Coded by <a href="https://github.com/DiMeCaCe/React04">Diana Cabral</a></h3>
       <Weather />
     </div>
   );
